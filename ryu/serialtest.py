68580006a9aa565f15978db0a3ae45280ce8f2cb
JOÃO SANTOS <116624654+Gingseng@users.noreply.github.com>
2022-11-17 10:57:56 -0400

    Update serialtest.py

--- ryu/serialtest.py
+++ ryu/serialtest.py
@@ -1,13 +1,10 @@
-import os
-import time
-from datetime import *
 
+import os
+from datetime import *
 import serial
 import configparser
-
+import time
 #leitura e declaração de arquivos
-arquivo = open('logs.txt', 'r')
-linha = arquivo.readline()
 
 cfgini = configparser.ConfigParser()
 cfgini.read('config.ini')
@@ -32,20 +29,6 @@ print ('StopBits        =', serialObj.stopbits)
 dataehora = datetime.now()
 stringer = dataehora.strftime("%d/%m/%Y %H:%M")
 
-
-
-print ("\n <<<<\\-----------------test de campo------------------//>>>>\n")
-print (stringer, "\n")
-print ("Lendo porta... \n")
-time.sleep (3)
-print ("Designed port set     ->",serialObj.port,)
-time.sleep (2)
-print ("DEFAULT BYTESIZE SET   ->",serialObj.bytesize)
-time.sleep (2)
-print ("Default parity:", serialObj.parity,)
-time.sleep (2)
-print ("DEFAULT STOP BITS SETED -> ", str(serialObj.stopbits))
-time.sleep (2)
 os.system ("cls")
 
 baudrate = serialObj.baudrate
@@ -70,59 +53,37 @@ print ('StopBits        =', serialObj.stopbits)
 
 #---------------------------------------------------------------
 
-#aqui irá ocorrer o salvamento das variaveis em um arquivo log
-loglead = dataehora("%d-%m")
-logleadp = loglead+'.log'
-file = open(logleadp,"a")
-file.write("---------logs---------- \n")
-file.write("Date/Time: ")
-file.write(stringer)
-file.write ("\n")
-file.write("Baudrate        =")
-file.write ( str (baudrate) )
-file.write ("\n")
-file.write("Port            =")
-file.write ( porta1 )
-file.write ("\n")
-file.write("ByteSize        =")
-file.write(str(bytesz))
-file.write ("\n")
-file.write("Parity          =")
-file.write (str(paridade) )
-file.write ("\n")
-file.write("StopBits        =")
-file.write(str(stop))
-file.write ("\n")
-time.sleep(1)
+
+
 
 def writereading(x):
     serialObj.write(bytes(x, 'utf-8'))
     time.sleep(0.05)
     dates = (str(serialObj.readline()))
     return dates
-contador = 0
-while True(contador<10):
+
+while True:
     value = writereading("x") 
-    contador = contador+1
-    print(("Data:--------------------"),value, contador ) 
-   
-    if (contador == 10):
-        os.system("cls")
-
-
-
+    print(("entry:"),value) 
     if (value != ("b''")):
-        file = open("logs.txt","a")
-        file.write("Valor de leitura        =", stringer)
-        file.write ("\n")
-        file.write (stringer)
-        file.write ("\n")
-        file.write(str(value))
-        file.write ("\n")
-        file.write("------------------------------- \n")
-        file.close()
+        
+
+#aqui irá ocorrer o salvamento das variaveis em um arquivo log
+        loglead = dataehora.strftime("%d-%m")
+        logleadp = loglead+'.log'
+        file = open(logleadp,"a")
+#17/11/2022 10:03, Baudrate: 9600, Port: COM5, ByteSize: 8,Parity: N,StopBits: 1
+        
+        file.writelines(stringer)
+        file.writelines("Setup de porta serial: ")
+        file.write("Port: ")
+        file.write ( porta1 )
+        file.write (" ,")
+        file.write("Valor de leitura:")
+        file.write(value)
+        file.write (" ,")
+        file.write ("saving sucess")
+        file.write("\n")
     else:
         os.system ("cls")
-
-
 #ATUALIZADO 10/11/2022 - João Santos 
